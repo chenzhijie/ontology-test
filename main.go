0939a152d0ac058582f0341362ce0077ed0a93ea
chenzhijie <chenzhijie@onchain.com>
2018-05-29 15:59:13 +0800

    support custom tps, stop timer channel
    Signed-off-by: chenzhijie <chenzhijie@onchain.com>

--- main.go
+++ main.go
@@ -14,34 +14,61 @@ import (
 var defAccount *account.Account
 var sdk *ontSdk.OntologySdk
 var toAddrs []common.Address
+var repeat int // repeat times for transfer to a address
+var noEnoughFound int
+var stopTimerCh chan bool
 
 const (
 	LOCAL_RPC_ADDRESS = "http://localhost:30336"
 )
 
 const (
-	TRANSFER_ONT_DURATION = 1 // transfer ont duration in second
-	TRANSFER_AMOUNT       = 10
+	TRANSFER_ONT_DURATION     = 1 // transfer ont duration in second
+	TRANSFER_AMOUNT           = 1
+	ONT_TPS                   = 200 // transaction per second
+	NO_ENOUGH_FOUND_MAX_CHECK = 600 // max check 0 balance times, if reach, stop the timer
 )
 
 func main() {
-	toAddrs = getToAddrs()
-	if toAddrs == nil || len(toAddrs) == 0 {
-		log.Warnf("no transfer to address")
+	ret := initVars()
+	if !ret {
+		log.Error("init instance variable failed")
 		return
 	}
-	sdk = ontSdk.NewOntologySdk()
-	sdk.Rpc.SetAddress(LOCAL_RPC_ADDRESS)
-	log.InitLog(0, log.PATH, log.Stdout)
-	defAccount = account.NewAccount("")
-	log.Infof("default account address:%v", defAccount.Address.ToBase58())
 	timer := time.NewTicker(time.Duration(TRANSFER_ONT_DURATION * time.Second))
 	for {
 		select {
 		case <-timer.C:
-			transferOnt()
+			go transferOnt()
+		case <-stopTimerCh:
+			log.Info("stop timer because no enough found")
+			timer.Stop()
+			goto FINISHED
 		}
 	}
+FINISHED:
+	log.Info("finished")
+}
+
+func initVars() bool {
+	log.InitLog(0, log.PATH, log.Stdout)
+
+	toAddrs = getToAddrs()
+	if toAddrs == nil || len(toAddrs) == 0 {
+		log.Warnf("no transfer to address")
+		return false
+	}
+	repeat = (int)(ONT_TPS / len(toAddrs))
+	log.Infof("Transfer address count:%d, each address repeat %d", len(toAddrs), repeat)
+	noEnoughFound = 0
+	stopTimerCh = make(chan bool, 1)
+
+	sdk = ontSdk.NewOntologySdk()
+	sdk.Rpc.SetAddress(LOCAL_RPC_ADDRESS)
+
+	defAccount = account.NewAccount("")
+	log.Infof("default account address:%v", defAccount.Address.ToBase58())
+	return true
 }
 
 func transferOnt() {
@@ -51,18 +78,27 @@ func transferOnt() {
 	}
 	if bal.Ont == 0 || bal.Ont < TRANSFER_AMOUNT {
 		log.Warnf("no enough ont, balance:%d", bal.Ont)
+		noEnoughFound++
+		if noEnoughFound > NO_ENOUGH_FOUND_MAX_CHECK {
+			stopTimerCh <- true
+		}
 		return
 	}
+	counter := 0
 	for _, toAddr := range toAddrs {
-		txHash, err := sdk.Rpc.Transfer(0, 30000, "ONT", defAccount, toAddr, TRANSFER_AMOUNT)
-		if err != nil {
-			log.Errorf("transfer error:%s", err)
-			continue
+		for i := 0; i < repeat; i++ {
+			txHash, err := sdk.Rpc.Transfer(0, 30000, "ONT", defAccount, toAddr, TRANSFER_AMOUNT)
+			if err != nil {
+				log.Errorf("transfer error:%s", err)
+				continue
+			}
+			log.Infof("%d: txHash:%x, to:%s", counter, txHash.ToArray(), toAddr.ToBase58())
+			counter++
 		}
-		log.Infof("tx success txHash:%x", txHash.ToArray())
 	}
 }
 
+// read address from file
 func getToAddrs() []common.Address {
 	file, err := ioutil.ReadFile("./addrs")
 	if err != nil {
